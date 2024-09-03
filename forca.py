9f50a752ae5429c8f20bf778ac86c531304e18bf
sugabsss <72110631+sugabsss@users.noreply.github.com>
2024-09-03 13:08:02 -0300

    Finalização do jogo da forca!

--- forca.py
+++ forca.py
@@ -1,32 +1,132 @@
-import sys
+import sys,csv
 
-def palavra_forca():
-    try:
+palavras_forca = []
+class Forca:
+    def __init__(self):
+    #Criando inputs para o usúario criar palavras e colocar seus respectivos nomes;
+        self.jogador1 = ''
+        self.jogador2 = ''
+        self.palavra = ''
+    
+    #Set de um número máximo de tentativas possiveis;
+        self.tentativas = 5      
+        
+    #Conjuto de letras que ainda precisam ser encontradas;
+        self.letras_restantes = set()
+
+    #Conjuto de letras que já foram encontradas;
+        self.letras_encotradas = set()  
+
+    def usuario1(self):      #Função de criação de usúario 1;
         while True:
-            print("»»»»»» Vamos Jogar O Jogo da Forca! ««««««")
-            pergunta = input("Insira a Palavra da forca: ")
-            return pergunta
-    except ValueError as e:
-        print(f"Error{e}: Insira uma palavra válida")
+                print("»»»»»» Vamos Jogar O Jogo da Forca! ««««««")
+                self.jogador1 = input("Nome do jogador 1: ").strip()
 
-def resposta_forca():
-    try:
+                if self.jogador1:
+                    break
+                else: 
+                    print("Digite um nome de usúario Válido Jogador 1 : ")
+                    
+    def usuario2(self):     #Função de criação de usúario 2;
         while True:
-            letras = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-            letra = input("Insira uma letra de 'a' até 'z': ")
+                self.jogador2 = input("Nome do jogador 2: ").strip()
 
-            if letra < 'a' or letra > 'z':
-                print("Valor invalido!")
-                sys.exit(1)
+                if self.jogador2:
+                    print(f"\n»»»»»» Regras: ««««««\n 1. Você possui apenas 5 chances, caso erre mais de 5 vezes perderá;\n 2. Não é possivel selecionar caracteres especiais ou numeros;\n 3. Categorias: Tipos de Dados, Linguagens/Frameworks de Programação, Arquitetura Computacional;\n 4. Boa sorte {self.jogador2};\n ")
+                    break
+                else: 
+                    print("Digite um nome de usúario Válido Jogador 2 : ")
+    
+    def pergunta_forca(self):       #Função para pegar a palavra forca que foi escrita pelo usúario 1;
+        while True:
+            self.palavra = input(f"----> ATENÇÃO : (Lembre-se de respeitar a lista de categorias) <-----\n    »»»»» {self.jogador1}, digite a palavra da forca: «««««\n").lower().strip()
+            
+            self.letras_restantes = set(self.palavra)
+        
+            if self.palavra:   
+                return {'palavra':self.palavra}
+            else:
+                print("Coloque uma palavra Válida!")
+    
+    def tentativas_forca(self):         #Função para estabelecer um limite de tentativas e um limite de caracteres de entrada;
+        print(f" ATENÇÃO *  Tamanho da Palavra Escrita pelo ({self.jogador1}) é : ----> {len(self.palavra)} <----")
+        
+        while self.tentativas > 0:
+            self.resposta = input(f"Coloque uma letra : ").lower().strip()
 
-            if letra in letras:
-                print(f"Letra selecionada: {letra}")
+            if len(self.resposta) != 1 or not ('a' <= self.resposta <= 'z'):
+                    print("> Desculpe, letra inválida. Por favor digite uma letra entre 'a' e 'z'.")
+                    continue    
+                
+            if self.resposta in self.letras_restantes:
+                self.letras_encotradas.add(self.resposta)
+                self.letras_restantes.remove(self.resposta)
+                print(f"A letra '{self.resposta}' foi encontrada na palavra forca;")
+            
+            if not self.letras_restantes:
+                print("Parabéns! Você encontrou todas as letras.")
                 break
 
-    except ValueError as e:
-        print(f"Error{e}: Insira uma palavra válida")
+            try:
+                posicao = self.palavra.index(self.resposta)
+                print(f"A letra '{self.resposta}' está na posição: {posicao}")
+            
+            except ValueError:
+                print(f"A letra '{self.resposta}' não foi encontrada na palavra.")
+                self.tentativas -= 1
+                print(f"» » » » »  Você tem {self.tentativas} tentativas restantes.« « « « «\n")
+
+        if self.tentativas == 0:
+            print(f"Excesso no número de tentativas! Você perdeu a palavra era '{self.palavra}'.")
+            sys.exit(1)
+            
+def palavra_update(): 
+    try:
+        with open("forca.csv", mode = 'r') as file:
+            reader = csv.DictReader(file)
+            global palavras_forca
+
+            for row in reader:
+                row["palavra"] = (row["palavra"])
+                palavras_forca.append(row)
+                                      
+    except FileNotFoundError:
+        print("Error 404 FIle not FoundError")
+        return 
+    
+    except Exception as e:  
+        print(f"Error ao ler arquivo:{e}")
+        return 
+
+def palavra_salvar():
+    fieldnames = ["palavra"]
+
+    try:
+        with open("forca.csv", mode = 'w') as file:
+            write = csv.DictWriter(file, fieldnames=fieldnames)
+            write.writeheader()
+            write.writerows(palavras_forca)
+            
+    except FileNotFoundError:
+        print("Error 404 FIle not FoundError")
+        return 
+    
+    except Exception as e:  
+        print(f"Error ao ler arquivo:{e}")
+        return 
     
 def main():
-    palavra = palavra_forca()
-    resposta = resposta_forca()
+    forca = Forca()
+    forca.usuario1()
+    forca.usuario2()
+    palavra_update()
+
+    
+    palavra = forca.pergunta_forca()
+    forca.tentativas_forca()
+    palavras_forca.append(palavra)
+
+    palavra_salvar() 
+
+main()
 
